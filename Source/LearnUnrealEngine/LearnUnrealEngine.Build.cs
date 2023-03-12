eb5b218160af55e60faf376149b05c3e78651c65
CyberCat <Vgnetnet156@gmail.com>
2023-03-12 13:33:27 +0300

    Edit Build file

--- Source/LearnUnrealEngine/LearnUnrealEngine.Build.cs
+++ Source/LearnUnrealEngine/LearnUnrealEngine.Build.cs
@@ -12,12 +12,17 @@ public class LearnUnrealEngine : ModuleRules
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+        // tanis - start faster compile time for small projects
+        MinFilesUsingPrecompiledHeaderOverride = 1;
+        bFasterWithoutUnity = true;
+        // tanis - end
 
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
-	}
+        // Uncomment if you are using Slate UI
+        // PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+        // Uncomment if you are using online features
+        // PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+        // To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+    }
 }
